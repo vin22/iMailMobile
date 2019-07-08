185a7576623baa5d28ce9be1f806b70a21f6de01
vin22 <limvin_39@yahoo.com>
2019-07-08 14:17:05 +0700

    API_URL

--- app/src/main/java/com/application/imail/remote/APIUtils.java
+++ app/src/main/java/com/application/imail/remote/APIUtils.java
@@ -6,7 +6,7 @@ public class APIUtils {
 
     }
 
-    public static final String API_URL = "http://192.168.43.234/WebApi/api/";
+    public static final String API_URL = "http://192.168.43.234/iMail/api/";
     //192.168.100.5
     //192.168.43.38
     public static UserService getUserService(){
